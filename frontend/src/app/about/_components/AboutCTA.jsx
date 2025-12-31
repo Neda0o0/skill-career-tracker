dca030418089683710347be3ad20896ed7d7ed62
unknown <imany997@gmail.com>
2025-12-31 13:03:54 +0330

    Edit AboutCTA's style

--- frontend/src/app/about/_components/AboutCTA.jsx
+++ frontend/src/app/about/_components/AboutCTA.jsx
@@ -2,19 +2,28 @@ import Button from "@/components/ui/Button";
 
 const AboutCTA = () => {
   return (
-    <section>
-      <div>
-        {/* Headline */}
-        <h2>Ready to bring clarity to your learning?</h2>
+    <section className="w-full bg-linear-to-r from-[#6125A7] to-[#1E95E5]">
+      <div className="max-w-7xl mx-auto px-4 flex flex-col">
+        <div className="p-5 mt-5 flex flex-col gap-8 justify-center items-center mb-3">
+          {/* Headline */}
+          <h2 className="text-2xl md:text-4xl font-bold text-white">
+            Ready to Build Your Skills?
+          </h2>
 
-        {/* Supporting line */}
-        <p>
-          SkillTrack helps you stay focused, understand your progress, and move
-          forward with confidence.
-        </p>
+          {/* Supporting line */}
+          <p className="text-white">
+            SkillTrack helps you stay focused, understand your progress, and
+            move forward with confidence.
+          </p>
 
-        {/* Button */}
-        <Button variant="secondary">Create your account</Button>
+          {/* Buttons */}
+          <div className="flex gap-4">
+            <Button variant="secondary">Start Your Journey</Button>
+            <Button variant="secondaryOutline" className="text-white">
+              Explore Courses
+            </Button>
+          </div>
+        </div>
       </div>
     </section>
   );
