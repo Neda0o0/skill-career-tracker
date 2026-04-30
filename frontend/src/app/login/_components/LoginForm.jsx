0102bd6e216ed07433daf3fa2d0c9722f6d0c10e
unknown <imany997@gmail.com>
2026-04-30 16:35:32 +0330

    Edit login and register pages

--- frontend/src/app/login/_components/LoginForm.jsx
+++ frontend/src/app/login/_components/LoginForm.jsx
@@ -1,29 +1,33 @@
 import Button from "@/components/ui/Button";
+import Link from "next/link";
 
 const LoginForm = () => {
   return (
     <section className="flex items-start md:items-center justify-center px-4">
-      <div className="w-full max-w-md bg-white rounded-xl shadow-lg p-6 md:p-8 flex flex-col gap-8">
+      <div className="w-full max-w-md glass rounded-xl shadow-lg p-6 md:p-8 flex flex-col gap-8">
         {/* Header */}
         <header className="text-center space-y-2">
-          <h2 className="text-3xl md:text-4xl font-bold text-gray-900">
+          <h2 className="text-3xl md:text-4xl font-bold text-gray-600 dark:text-white text-shadow-md">
             Welcome back
           </h2>
-          <p className="text-gray-500">Sign in to continue your journey</p>
+          <p className="text-gray-500 dark:text-white/50">
+            Sign in to continue your journey
+          </p>
         </header>
 
         {/* Social Login */}
         <div className="flex flex-col gap-3">
-          <Button className="w-full">Continue with Google</Button>
-          <Button className="w-full">Continue with GitHub</Button>
+          <Button className="w-full glass">Continue with Google</Button>
+          <Button className="w-full glass">Continue with GitHub</Button>
         </div>
 
         {/* Divider */}
-        <div className="relative flex items-center justify-center">
-          <span className="absolute inset-x-0 h-px bg-gray-200" />
-          <span className="relative bg-white px-3 text-sm text-gray-400">
+        <div className="flex items-center gap-3 text-sm text-gray-400">
+          <span className="flex-1 h-px bg-gray-200"></span>
+          <Link href="/register" className="text-gray-600 dark:text-white/50">
             or sign in with email
-          </span>
+          </Link>
+          <span className="flex-1 h-px bg-gray-200" />
         </div>
 
         {/* Form */}
@@ -32,7 +36,7 @@ const LoginForm = () => {
           <div className="flex flex-col gap-1.5">
             <label
               htmlFor="email"
-              className="text-sm font-medium text-gray-600"
+              className="text-sm font-medium text-gray-600 dark:text-white/50"
             >
               Email
             </label>
@@ -50,7 +54,7 @@ const LoginForm = () => {
           <div className="flex flex-col gap-2">
             <label
               htmlFor="password"
-              className="text-sm font-medium text-gray-600"
+              className="text-sm font-medium text-gray-600 dark:text-white/50"
             >
               Password
             </label>
@@ -66,7 +70,7 @@ const LoginForm = () => {
 
           {/* Remember / Forgot */}
           <div className="flex items-center justify-between text-sm">
-            <label className="flex items-center gap-2 text-gray-600">
+            <label className="flex items-center gap-2 text-gray-600 dark:text-white/50">
               <input type="checkbox" className="accent-blue-600" />
               Remember me
             </label>
@@ -76,14 +80,14 @@ const LoginForm = () => {
           </div>
 
           {/* CTA */}
-          <Button className="w-full py-3 text-base font-semibold">
+          <Button className="w-full py-3 text-base font-semibold glass">
             Sign In
           </Button>
         </form>
 
         {/* Switch Auth */}
-        <div className="text-sm text-center text-gray-500">
-          Do not have account?
+        <div className="text-sm text-center text-gray-300">
+          Do not have any account?
           <a className="ml-1 text-blue-500 cursor-pointer hover:underline">
             sign up
           </a>
--- frontend/src/app/register/_components/RegisterForm.jsx
+++ frontend/src/app/register/_components/RegisterForm.jsx
@@ -1,27 +1,42 @@
+"use client";
+
 import Button from "@/components/ui/Button";
+import Link from "next/link";
+import { useState } from "react";
 
 const RegisterForm = () => {
+  const [password, setPassword] = useState("");
+  const validations = {
+    length: password.length >= 8,
+    uppercase: /[A-Z]/.test(password),
+    number: /[0-9]/.test(password),
+  };
+
   return (
     <section className=" flex items-center justify-center">
-      <div className="w-full max-w-md bg-white rounded-xl mt-4 mb-4 shadow-lg p-6 md:p-8 flex flex-col gap-8">
+      <div className="w-full max-w-md glass rounded-xl mt-4 mb-4 shadow-lg p-6 md:p-8 flex flex-col gap-8">
         {/* Header */}
         <header className="text-center space-y-2">
-          <h2 className="text-3xl md:text-4xl font-bold text-gray-900">
+          <h2 className="text-3xl md:text-4xl font-bold text-gray-600 dark:text-white text-shadow-md">
             Create your account
           </h2>
-          <p className="text-gray-500">Start tracking your skills today</p>
+          <p className="text-gray-600 dark:text-white/50 mt-4">
+            Start tracking your skills today
+          </p>
         </header>
 
         {/* Social Signup */}
         <div className="flex flex-col gap-3">
-          <Button className="w-full">Sign up with Google</Button>
-          <Button className="w-full">Sign up with GitHub</Button>
+          <Button className="w-full glass">Sign up with Google</Button>
+          <Button className="w-full glass">Sign up with GitHub</Button>
         </div>
 
         {/* Divider */}
         <div className="flex items-center gap-3 text-sm text-gray-400">
           <span className="flex-1 h-px bg-gray-200"></span>
-          or continue with email
+          <Link href="/login" className="text-gray-600 dark:text-white/50 ">
+            or continue with email
+          </Link>
           <span className="flex-1 h-px bg-gray-200" />
         </div>
 
@@ -30,7 +45,9 @@ const RegisterForm = () => {
           {/* Name */}
           <div className="grid grid-cols-2 gap-4">
             <div className="flex flex-col gap-1">
-              <label className="text-gray-500">First Name</label>
+              <label className="text-gray-600 dark:text-white/50">
+                First Name
+              </label>
               <input
                 type="text"
                 placeholder=" First Name"
@@ -39,7 +56,9 @@ const RegisterForm = () => {
             </div>
 
             <div className="flex flex-col gap-1">
-              <label className="text-gray-500">Last Name</label>
+              <label className="text-gray-600 dark:text-white/50">
+                Last Name
+              </label>
               <input
                 type="text"
                 placeholder=" Last Name"
@@ -50,7 +69,9 @@ const RegisterForm = () => {
 
           {/* Email */}
           <div className="flex flex-col gap-1">
-            <label className="text-gray-500">Email Address</label>
+            <label className="text-gray-600 dark:text-white/50">
+              Email Address
+            </label>
             <input
               type="email"
               placeholder=" Email Address"
@@ -60,22 +81,45 @@ const RegisterForm = () => {
 
           {/* Password */}
           <div className="flex flex-col gap-2">
-            <label className="text-gray-500">Password</label>
+            <label className="text-gray-600 dark:text-white/50">Password</label>
             <input
               type="password"
               placeholder=" Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500/40 focus:border-blue-500"
             />
-            <ul className="text-xs text-gray-500 space-y-1 ml-1">
-              <li>At least 8 characters</li>
-              <li>One uppercase letter</li>
-              <li>One number</li>
-            </ul>
+            {/* شروط اعتبارسنجی */}
+            <div className="flex gap-4 text-xs mt-2 flex-wrap justify-center">
+              <span
+                className={`flex items-center gap-1 ${
+                  validations.length ? "text-green-500" : "text-gray-400"
+                }`}
+              >
+                {validations.length ? "✔️" : ""} At least 8 characters
+              </span>
+              <span
+                className={`flex items-center gap-1 ${
+                  validations.uppercase ? "text-green-500" : "text-gray-400"
+                }`}
+              >
+                {validations.uppercase ? "✔️" : ""} One uppercase letter
+              </span>
+              <span
+                className={`flex items-center gap-1 ${
+                  validations.number ? "text-green-500" : "text-gray-400"
+                }`}
+              >
+                {validations.number ? "✔️" : ""} One number
+              </span>
+            </div>
           </div>
 
           {/* Confirm Password */}
           <div className="flex flex-col gap-1">
-            <label className="text-gray-500">Confirm Password</label>
+            <label className="text-gray-600 dark:text-white/50">
+              Confirm Password
+            </label>
             <input
               type="password"
               placeholder=" Confirm Password"
@@ -86,7 +130,7 @@ const RegisterForm = () => {
           {/* Optional Info */}
           <div className="grid grid-cols-2 gap-4">
             <div className="flex flex-col gap-1">
-              <label className="text-gray-500">
+              <label className="text-gray-600 dark:text-white/50">
                 Job Title <span className="opacity-60">(Optional)</span>
               </label>
               <input
@@ -97,7 +141,7 @@ const RegisterForm = () => {
             </div>
 
             <div className="flex flex-col gap-1">
-              <label className="text-gray-500">
+              <label className="text-gray-600 dark:text-white/50">
                 Current Company <span className="opacity-60">(Optional)</span>
               </label>
               <input
@@ -110,8 +154,8 @@ const RegisterForm = () => {
 
           {/* Terms */}
           <div className="flex items-start gap-2 text-sm">
-            <input type="checkbox" className="mt-1 accent-blue-600" />
-            <p className="text-sm text-gray-600">
+            <input type="checkbox" className="mt-1 accent-blue-600 glass" />
+            <p className="text-sm text-gray-600 dark:text-white/50">
               I agree to the
               <a className="text-blue-400 cursor-pointer">Terms of Service </a>
               and
@@ -122,7 +166,7 @@ const RegisterForm = () => {
           {/* CTA */}
           <Button
             variant="secondary"
-            className="w-full py-3 text-base font-semibold"
+            className="w-full py-3 text-base font-semibold glass"
           >
             Create Account
           </Button>
@@ -130,20 +174,22 @@ const RegisterForm = () => {
 
         {/* Footer */}
         <div className="text-sm text-center space-y-2 text-gray-500">
-          <p>
+          <p className="text-gray-600 dark:text-white/50">
             Already have an account?
             <a className="cursor-pointer text-blue-400"> sign in</a>
           </p>
-          <p className="opacity-70">Your data is secure and encrypted</p>
+          <p className="opacity-70 text-gray-600 dark:text-white/50">
+            Your data is secure and encrypted
+          </p>
         </div>
 
         {/* Legal */}
         <footer className="flex justify-center gap-4 text-xs text-gray-400">
           <span>
-            Privacy Policy <em className="m-2">|</em>
+            Privacy Policy <em className="m-2 text-white/50">|</em>
           </span>
           <span>
-            Terms<em className="m-2">|</em>
+            Terms<em className="m-2 text-white/50">|</em>
           </span>
           <span>Help</span>
         </footer>
